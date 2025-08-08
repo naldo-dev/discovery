16c58ed7ee00da541fb83faca0d253a756346429
naldo-dev <naldovasconcelosdev@gmail.com>
2025-08-08 18:07:56 -0300

    Versão Beta

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, com cabelo curto e barba, usando uma camiseta preta e um casaco cinza."
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+    )
+  }
+}
